e723e1ab5846684d9c81a380b27be500270149c6
Temilade <afolstee@gmail.com>
2025-06-14 18:39:34 +0100

    removed unneccesary files

--- json.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('date-selector-button').addEventListener('click', function() {
-    console.log('Button clicked'); // Add this line
-    var dateInputs = document.querySelector('.date-selector .date-inputs');
-    dateInputs.classList.toggle('active');
-});
